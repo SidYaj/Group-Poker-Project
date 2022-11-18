db2e93699f1cf80193f88068537d9fc65a3fd59b
SidYaj <66437960+SidYaj@users.noreply.github.com>
2022-11-18 17:18:38 +0800

    Update combinations.h

--- combinations.h
+++ combinations.h
@@ -5,6 +5,6 @@
 #include "player.h"
 #include "community.h"
 
-std::vector<std::vector<int>> combinations(std::vector<int> cards); // to find all 5 card combinations to pass to function check_hand to see what hand a player has
+std::vector<std::vector<int> > combinations(std::vector<int> cards); // to find all 5 card combinations to pass to function check_hand to see what hand a player has
 
 #endif
